33fbe254ad5a6ec73a3a8bfdcc82ecf393a095a9
NoahHDesktop <NoahH@DESKTOP-JAEBKSV>
2024-10-17 10:50:50 +0200

    Add project files.

---
+++ GrundernaOOP/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Circle
+    {
+        private double radius;
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+        public double GetArea()
+        {
+            double circleArea = Math.Round(radius * radius * Math.PI, 2);
+            return circleArea;
+        }
+        public double GetCircumference()
+        {
+            double circleCircumference = Math.Round(2 * Math.PI * radius, 2);
+            return circleCircumference;
+        }
+        public double GetSphereVolume()
+        {
+            double sphereVolume = Math.Round(4 / 3 * Math.PI * radius * radius * radius,2);
+            return sphereVolume;
+        }
+    }
+}
---
+++ GrundernaOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cirkel1 = new Circle(5);
+            Circle cirkel2 = new Circle(6);
+
+            Console.WriteLine($"Cirkel 1 area är: {cirkel1.GetArea()}");
+            Console.WriteLine($"Cirkel 2 area är: {cirkel2.GetArea()}");
+            Console.WriteLine("");
+            Console.WriteLine($"Cirkel 1 omkrets är: {cirkel1.GetCircumference()}");
+            Console.WriteLine($"Cirkel 2 omkrets är: {cirkel2.GetCircumference()}");
+            Console.WriteLine("");
+            Console.WriteLine($"Cirkel 1 var en sfär, så skulle volymen vara: {cirkel1.GetSphereVolume()}");
+            Console.WriteLine($"Cirkel 2 var en sfär, så skulle volymen vara: {cirkel2.GetSphereVolume()}");
+        }
+    }
+}
